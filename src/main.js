cfacf688e8f9fd25935f346385f34691ec3c69b2
teamcutter <ruslan.sibgatulin05@gmail.com>
2022-03-02 13:37:24 +0600

    int

--- src/main.js
+++ src/main.js
@@ -4,6 +4,6 @@ import router from './router'
 import store from './store'
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://172.31.84.228:80'
+axios.defaults.baseURL = 'https://apirugmo-website.herokuapp.com/'
 
 createApp(App).use(store).use(router, axios).mount('#app')
